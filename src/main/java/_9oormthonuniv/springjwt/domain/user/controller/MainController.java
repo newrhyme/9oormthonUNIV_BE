4ab6450057a2a289d5162033fbabbfbb6b463dfa
newrhyme <ksh000624@naver.com>
2025-05-06 10:20:57 +0900

    [FEAT] : MainController에서 로그인 사용자 정보(username, role) 반환

--- src/main/java/_9oormthonuniv/springjwt/domain/user/controller/MainController.java
+++ src/main/java/_9oormthonuniv/springjwt/domain/user/controller/MainController.java
@@ -1,8 +1,14 @@
 package _9oormthonuniv.springjwt.domain.user.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 @Controller
 @ResponseBody
@@ -10,6 +16,19 @@ public class MainController {
     @GetMapping
 
     public String MainP() {
-        return "Main Controller";
+
+        // 세션 현재 사용자 아이디
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        // 세션 현재 사용자 role
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        // Collection을 Iterator 반복자를 통해서 role 값 빼는 로직
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+        GrantedAuthority auth = iterator.next();
+        String role = auth.getAuthority();
+
+        return "Main Controller " + username + " " + role;
     }
 }
